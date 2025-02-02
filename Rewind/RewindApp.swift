5d53c4746f3ecbaecaaadd4584a3abd886b43a18
Alex Sherstnev <alexxingplus@inbox.ru>
2025-02-02 21:43:59 +0100

    Initial Commit

---
+++ Rewind/RewindApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewindApp.swift
+//  Rewind
+//
+//  Created by Alexey Sherstnev on 02.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct RewindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RewindTests/RewindTests.swift
@@ -0,0 +1,17 @@
+//
+//  RewindTests.swift
+//  RewindTests
+//
+//  Created by Alexey Sherstnev on 02.02.2025.
+//
+
+import Testing
+@testable import Rewind
+
+struct RewindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
