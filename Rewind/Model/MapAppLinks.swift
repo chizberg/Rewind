d0269d4a09e544ce592e2059b30acf301de548cb
Alex Sherstnev <43221673+chizberg@users.noreply.github.com>
2025-12-03 02:24:05 +0100

    issue #41: fix route deeplinks (#59)

--- Rewind/Model/MapAppLinks.swift
+++ Rewind/Model/MapAppLinks.swift
@@ -11,7 +11,6 @@ enum MapApp: CaseIterable {
   case apple
   case google
   case yandex
-  case gis
 }
 
 extension MapApp {
@@ -20,35 +19,17 @@ extension MapApp {
     case .apple: "Apple Maps"
     case .google: "Google Maps"
     case .yandex: "Yandex Maps"
-    case .gis: "2GIS"
-    }
-  }
-
-  var downloadLink: URL? {
-    switch self {
-    case .yandex:
-      URL(string: "https://apps.apple.com/ru/app/id313877526")
-    case .gis:
-      URL(string: "https://itunes.apple.com/ru/app/id481627348?mt=8")
-    case .google:
-      URL(string: "https://apps.apple.com/ru/app/id585027354")
-    case .apple:
-      nil
     }
   }
 
   func coordinateLink(latitude: Double, longitude: Double) -> URL? {
     switch self {
-    case .yandex:
-      URL(string: "yandexmaps://yandex.ru/maps/?rtext=~\(latitude)%2C\(longitude)")
-    case .gis:
-      URL(
-        string: "dgis://2gis.ru/routeSearch/rsType/pedestrian/to/\(longitude),\(latitude)"
-      )
-    case .google:
-      URL(string: "comgooglemaps://?daddr=\(latitude),\(longitude)")
     case .apple:
       URL(string: "http://maps.apple.com/?daddr=\(latitude),\(longitude)")
+    case .google:
+      URL(string: "https://www.google.com/maps/dir//\(latitude),\(longitude)")
+    case .yandex:
+      URL(string: "https://yandex.com/maps/?mode=routes&rtext=~\(latitude),\(longitude)&rtt=auto")
     }
   }
 }
